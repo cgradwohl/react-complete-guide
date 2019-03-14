0f503d9a5937e849a025a027ac0874a248e038a7
Chris Gradwohl <christophergradwohl@gmail.com>
2019-03-14 08:05:29 -0700

    useEffects hook runs for every update

--- guide/src/components/Cockpit/Cockpit.js
+++ guide/src/components/Cockpit/Cockpit.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import classes from './Cockpit.css';
 const cockpit = (props) => {
+  // Runs every update cycle
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect()');
+  });
   const assignedClasses = [];
   let btnClass = '';
   if (props.showPersons) {
@@ -9,7 +13,7 @@ const cockpit = (props) => {
 
   if (props.persons.length <= 2) assignedClasses.push(classes.green);
   if (props.persons.length <= 1) assignedClasses.push(classes.bold);
-  if (props.persons.length == 0) {
+  if (props.persons.length === 0) {
     const idx = assignedClasses.findIndex(el => el === 'green');
     assignedClasses[idx] = 'red';
   }
