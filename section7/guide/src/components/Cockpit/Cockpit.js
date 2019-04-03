faa74efba074072d051bd8f9f841f37dc64cfbc6
Chris Gradwohl <christophergradwohl@gmail.com>
2019-04-03 07:26:12 -0700

    burger builder updates

--- section7/guide/src/components/Cockpit/Cockpit.js
+++ section7/guide/src/components/Cockpit/Cockpit.js
@@ -1,23 +1,23 @@
-import React, {useState, useEffect, useRef, useContext} from 'react';
-import classes from './Cockpit.css';
-import AuthContext from '../../context/auth-context';
-const cockpit = (props) => {
+import React, { useState, useEffect, useRef, useContext } from "react";
+import classes from "./Cockpit.css";
+import AuthContext from "../../context/auth-context";
+const cockpit = props => {
   const toggleBtnRef = useRef(null);
 
   const authContextObj = useContext(AuthContext);
 
-  console.log('are we authed?', authContextObj.authed);
+  console.log("are we authed?", authContextObj.authed);
 
   // if want to base your state on props,
   // you can useSate(){} instead!
-  useState((state, stateHandler) => {
-
-  });
+  useState((state, stateHandler) => {});
 
   // Runs every render cycle (creation or )
   // componentDidMount() and ComponentDidUpdate() combined
   useEffect(() => {
-    console.log('[Cockpit.js] useEffect(), happens every render cycle, after mount');
+    console.log(
+      "[Cockpit.js] useEffect(), happens every render cycle, after mount"
+    );
     // this code runs when component did mount
     // setTimeout(() => {
     //   alert('DUDEBRO');
@@ -26,10 +26,10 @@ const cockpit = (props) => {
     // this code runs when dep un mounts based on the dep args list you pass
     return () => {
       // runs AFTER every render cycle
-      console.log('[Cockpit.js] Cleanup work');
+      console.log("[Cockpit.js] Cleanup work");
     };
-    // you can pass an array as a second arg 
-    // to useEffects which will tell it to 
+    // you can pass an array as a second arg
+    // to useEffects which will tell it to
     // run only when that dependency/props has changed.
     // pass empty array to only runs the first time!
   }, []);
@@ -41,16 +41,16 @@ const cockpit = (props) => {
 
   // runs for every render/update cycle
   useEffect(() => {
-    console.log('[Cockpit.js] 2nd useEffect');
+    console.log("[Cockpit.js] 2nd useEffect");
     // runs AFTER every render cycle
     return () => {
       // this runs for the last time
-      console.log('[Cockpit.js] Cleanup work in 2nd useEffect');
-    }
+      console.log("[Cockpit.js] Cleanup work in 2nd useEffect");
+    };
   });
 
   const assignedClasses = [];
-  let btnClass = '';
+  let btnClass = "";
   if (props.showPersons) {
     btnClass = classes.Green;
   }
@@ -58,23 +58,19 @@ const cockpit = (props) => {
   if (props.personsLength <= 2) assignedClasses.push(classes.green);
   if (props.personsLength <= 1) assignedClasses.push(classes.bold);
   if (props.personsLength === 0) {
-    const idx = assignedClasses.findIndex(el => el === 'green');
-    assignedClasses[idx] = 'red';
+    const idx = assignedClasses.findIndex(el => el === "green");
+    assignedClasses[idx] = "red";
   }
   return (
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
-      <p className={assignedClasses.join(' ')}>{props.otherState}</p>
-      <button
-        ref={toggleBtnRef}
-        className={btnClass}
-        onClick={props.clicked}>Toggle Persons
+      <p className={assignedClasses.join(" ")}>{props.otherState}</p>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
+        Toggle Persons
       </button>
-        {<button onClick={authContextObj.login}>Login
-      </button>}
-      
+      {<button onClick={authContextObj.login}>Login</button>}
     </div>
-  )
+  );
 };
 
 export default React.memo(cockpit);
